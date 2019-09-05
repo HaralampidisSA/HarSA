6d10fff122517ae0c7766af281539222a6d39604
alex <alex@thinknature.gr>
2019-09-05 12:28:11 +0300

    added json logging project

---
+++ HarSA.AspNetCore.Logging/JsonLoggerFactoryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HarSA.AspNetCore.Logging.Json
+{
+    public static class JsonLoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddJsonLogger(this ILoggerFactory factory, IHttpContextAccessor httpContextAccessor, string applicationName, string apiServiceUrl)
+        {
+            factory.AddProvider(new JsonLoggerProvider(httpContextAccessor, applicationName, apiServiceUrl));
+
+            return factory;
+        }
+    }
+}
