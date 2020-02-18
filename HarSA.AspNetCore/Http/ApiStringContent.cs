1c680de75daaaab648a70e0d6336f08e47502749
alex <alex@thinknature.gr>
2020-02-18 13:31:11 +0200

    added content

---
+++ HarSA.AspNetCore/Http/ApiStringContent.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Text;
+
+namespace HarSA.AspNetCore.Http
+{
+    public class JsonStringContent : StringContent
+    {
+        public JsonStringContent(string content) : this(content, Encoding.UTF8, "application/json")
+        {
+        }
+
+        public JsonStringContent(string content, Encoding encoding, string mediaType) : base(content, encoding, mediaType)
+        {
+        }
+    }
+}
